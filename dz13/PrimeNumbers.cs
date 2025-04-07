56421d8bdb157e193ddc10b84e48ec1c8fcc14bb
witchbladedovi4 <pidzes@vk.com>
2025-04-07 21:01:24 +0500

    1

---
+++ dz13/PrimeNumbers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace dz13
+{
+    public class PrimeNumbers
+    {
+        public static IEnumerable<int> GetPrimes(int limit)
+        {
+            if (limit < 2) yield break;
+            yield return 2;
+
+            for (int i = 3; i <= limit; i += 2)
+            {
+                if (IsPrime(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+        public static IEnumerable<int> GetAllPrimes()
+        {
+            yield return 2; 
+
+            int number = 3;
+            while (true)
+            {
+                if (IsPrime(number))
+                {
+                    yield return number;
+                }
+                number += 2; 
+            }
+        }
+
+
+        public static IEnumerable<int> SkipPrimes(int count)
+        {
+            int skipped = 0;
+            foreach (int prime in GetAllPrimes())
+            {
+                if (skipped >= count)
+                {
+                    yield return prime;
+                }
+                skipped++;
+            }
+        }
+
+        private static bool IsPrime(int a)
+        {
+            if (a < 2) return false;
+            if (a % 2 == 0) return a == 2;
+            int x = (int)Math.Sqrt(a);
+
+            for (int i = 3; i <= x; i += 2)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- dz13/Program.cs
+++ dz13/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            foreach (int prime in PrimeNumbers.GetPrimes(50))
+            {
+                Console.Write(prime + " ");
+            }
+            Console.WriteLine();
+            int count = 0;
+            foreach (int prime in PrimeNumbers.GetAllPrimes())
+            {
+                Console.Write(prime + " ");
+                count++;
+                if (count >= 10) break;
+            }
+            Console.WriteLine();
+            foreach (int prime in PrimeNumbers.SkipPrimes(10))
+            {
+                Console.Write(prime + " ");
+                if (prime > 30) break; 
+            }
+            Console.WriteLine();
         }
     }
 }
